7c55b9bd8f8fa678cdc515a1a2a9e2481ac6511d
Ben Dechrai <ben@dechrai.com>
2023-04-30 18:00:27 -0400

    Cleaned up the code

--- src/functions/pgp.js
+++ src/functions/pgp.js
@@ -12,15 +12,12 @@ export const generateKeys = async (name, email, passphrase) => {
 
 export const verifyPassphrase = async (keys, passphrase) => {
   try {
-    const publicKey = await openpgp.readKey({ armoredKey: keys.publicKey });
-    const privateKey = await openpgp.decryptKey({
+    await openpgp.decryptKey({
       privateKey: await openpgp.readPrivateKey({ armoredKey: keys.privateKey }),
       passphrase
     });
-    console.log(privateKey);
     return true;
   } catch (e) {
-    console.log(e);
     return false;
   }
 }
